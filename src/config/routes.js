51a702ba3036d16bfe46ab9b8ec00454a3e61057
brock <brock@pop-os.localdomain>
2020-05-31 20:19:06 -0700

    step 2: Adds a minimal GameShow page

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import GameList from '../pages/GameList'
+import GameShow from '../pages/GameShow'
 
 export default (
   <Switch>
     <Route exact path='/' component={ Home } />
+    <Route path='/games/:id' component={ GameShow } />
     <Route path='/games' component={ GameList } />
   </Switch>
 )
--- src/models/game.js
+++ src/models/game.js
@@ -4,6 +4,10 @@ class GameModel {
   static all = () => {
     return fetch(`${url}/games`).then(res => res.json())
   }
+
+  static show = (gameId) => {
+    return fetch(`${url}/games/${gameId}`).then(res => res.json())
+  }
 }
 
 export default GameModel
