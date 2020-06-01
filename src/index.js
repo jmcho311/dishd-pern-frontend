dd58eaf1ae217286fa2a45cc0be4cf045082eb1e
brock <brock@pop-os.localdomain>
2020-05-31 17:33:16 -0700

    Initial commit. Gamelib front end starter code complete.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  // StrictMode is a tool for highlighting potential 
+  // problems in an application. Like Fragment, 
+  // StrictMode does not render any visible UI. 
+  // It activates additional checks and warnings for 
+  // its descendants.
   <React.StrictMode>
-    <App />
+    <Router>  
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/models/game.js
@@ -0,0 +1,7 @@
+const url = `http://localhost:3001/api/v1`
+
+class GameModel {
+
+}
+
+export default GameModel
