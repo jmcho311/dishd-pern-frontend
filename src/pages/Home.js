abc34b8b08288a845552e68ba51fa450c76d815a
jmcho311 <jmcho311@gmail.com>
2020-12-16 21:33:31 -0800

    grid layout for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import './App.scss'
 
 const Home = () => {
   return (
-    <div>
-      <h3>bringing people and food together</h3>
-      <img src="../images/home.jpg" alt="breakfast"/>
-      <h3>a simple way to post and share your favorite food</h3>
-      <img src="../images/home2.jpg" alt="pasta"/>
+    <div className="home">
+      <Container>
+        <Row>
+          <Col sm={8}><img src="../images/home.jpg" alt="breakfast"/></Col>
+            {/* <img src="../images/home.jpg" alt="breakfast"/> */}
+          <Col sm={4}><h3>bringing people and food together</h3></Col>
+            {/* <h3>bringing people and food together</h3> */}
+        </Row>
+        <Row>
+          <Col sm={4}><h3>a simple way to post and share your favorite food</h3></Col>
+            {/* <h3>a simple way to post and share your favorite food</h3> */}
+          <Col sm={8}><img src="../images/home2.jpg" alt="pasta"/></Col>
+            {/* <img src="../images/home2.jpg" alt="pasta"/> */}
+        </Row>
+      </Container>
     </div>
-  );
+  )
 }
 
 export default Home
